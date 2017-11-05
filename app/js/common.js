7274d83aabac38705744e02c211f8fcbf1225d62
Eugene-S-90 <slon4ig@gmail.com>
2017-11-05 13:14:31 +0200

    first upload

---
+++ app/js/common.js
@@ -0,0 +1,107 @@
+
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
+
+function showNav(){
+  var x;
+  x = document.getElementsByClassName("main-nav")[0];
+  x.classList.toggle("show-menu");
+}
+
+function activateLogo(){
+  var y;
+  x = document.getElementsByClassName("logo-border")[0];
+  x.classList.toggle("logo-active");
+}
+
+window.onload = function(){
+  var menuHandler;
+  menuHandler = document.getElementsByClassName("logo")[0];
+  menuHandler.addEventListener("click", showNav);
+  menuHandler.addEventListener("click", activateLogo);
+  autoType(".type-js",150);
+}
+
+
+
+
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("contact-btn");
+
+document.getElementById("contact-btn").addEventListener("click", function(event){
+  event.preventDefault()
+});
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+
+      });
+    }
+  }
+});
+
+
+        
